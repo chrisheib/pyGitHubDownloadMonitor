43e37d7089a2caad1cf47dab8086bab7beaca97e
STSchiff <chrisheib@gmx.de>
2020-05-04 17:21:16 +0200

    implement total and totalDelta

--- DLHT_main.py
+++ DLHT_main.py
@@ -9,11 +9,14 @@ if __name__ == "__main__":
     conn = db.create_connection(r"pythonsqlite.db")
     db.create_tables(conn)
     response = api.getReleaseList()
+    totalCount = 0
     for release in response:
         print(release["name"])
         for asset in release["assets"]:
             print("   " + asset["name"] + "   " + str(asset["download_count"]))
             db.add_data(conn, datetime.today().strftime('%Y-%m-%d'), asset["name"], release["name"], asset["download_count"])
+            totalCount += asset["download_count"]
+    db.add_data(conn, datetime.today().strftime('%Y-%m-%d'), "total", "total", totalCount)
 
     conn.commit()
     plot.CreateGraph(conn)
--- DLHT_plot.py
+++ DLHT_plot.py
@@ -5,12 +5,12 @@ import DLHT_db as db
 import DLHT_config as conf
 
 def CreateGraph(connection):
-    fig = plt.figure(figsize=[15,6])
-    #ax = fig.add_subplot(121)
+    plt.figure(figsize=[15,6])
     ax = plt.subplot2grid((1, 3), (0, 0), colspan=2)
-    plotdata = []
 
-    result = connection.execute('SELECT ROWID, version, name FROM files ORDER BY version DESC, name')
+    # Get Plotdata
+    plotdata = []
+    result = connection.execute('SELECT ROWID, version, name FROM files WHERE version <> "total" ORDER BY version DESC, name')
     for row in result:
         data = db.select(connection, 'SELECT date, count FROM data WHERE filesID = ? order by date', [row[0]])
         data1 = []
@@ -29,13 +29,28 @@ def CreateGraph(connection):
             deltastring = " (+" + str(dlDelta) + ")"
         plotdata.append([data1,data2,row[1] + ' ' + row[2] + ': ' + str(finalDL) + deltastring])
 
+    # Create new line for every file in the database
     for line in plotdata:
         ax.plot(line[0],line[1],label=line[2])
 
     ax.set_ylabel('Downloads')
     ax.set_xlabel('Date')
     name, repo = conf.getRepoData()
-    ax.set_title(name+'/'+repo)
+
+    # Find total:
+
+    result = connection.execute('SELECT count FROM data LEFT JOIN files on filesID = files.ROWID WHERE version = "total" ORDER BY date DESC limit 2')
+    total = 0
+    lastTotal = 0
+    for row in result:
+        lastTotal = total
+        total = row[0]
+    deltaTotal = total - lastTotal
+    deltastring = ""
+    if (deltaTotal > 0):
+        deltastring = " (+" + str(deltaTotal) + ")"
+
+    ax.set_title(name+'/'+repo + ": " + str(total) + deltastring)
 
     ax.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
 
