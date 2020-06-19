3d72282f14f1d54cb4145e76dfd19a30266a5a69
STSchiff <chrisheib@gmx.de>
2020-06-19 22:03:10 +0200

    use real scriptpath for config

--- DLHT_config.py
+++ DLHT_config.py
@@ -1,18 +1,20 @@
 import configparser
+import os
 
 def getApiKey():
     config = configparser.ConfigParser()
-    config.read('conf.ini')
+    config.read(os.path.dirname(os.path.abspath(__file__))+'/conf.ini')
     return config["GitHub"]["APIKey"]
 
 def getRepoData():
     config = configparser.ConfigParser()
-    config.read('conf.ini')
+    print(os.path.dirname(os.path.abspath(__file__)))
+    config.read(os.path.dirname(os.path.abspath(__file__))+'/conf.ini')
     data = config["GitHub"]
     return data["Username"], data["RepositoryName"]
 
 def ShowPicture():
     config = configparser.ConfigParser()
-    config.read('conf.ini')
+    config.read(os.path.dirname(os.path.abspath(__file__))+'/conf.ini')
     data = config["Settings"]
     return data["OpenPictureOnRun"] == "True"
